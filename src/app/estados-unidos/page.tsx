de0ff5c9c89ab491cf48bc65d3db87d2832fe8de
ricardoabb <ricardo.batista@opeteducation.com.br>
2025-06-28 19:50:11 -0300

    🚀 usa page

--- src/app/estados-unidos/page.tsx
+++ src/app/estados-unidos/page.tsx
@@ -5,6 +5,7 @@ import Home from '../components/Layout';
 import { info } from '../utils/info';
 import MapMenuPrincipal from '../components/MapMenuPrincipal';
 import MapSVG from '../components/MapSVG';
+import Image from 'next/image';
 
 export default function EstadosUnidos() {
   
@@ -12,6 +13,9 @@ export default function EstadosUnidos() {
     return (
         <Home>
             <h1>Estados Unidos</h1>
+            <div className='flex justify-center items-center px-5'>
+            <Image src="/Piramide_EUA_2021_Vetorizada.png" alt="Estados Unidos" width={500} height={500} />
+            </div>
         </Home>
     );
 }
